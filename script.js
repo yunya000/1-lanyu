6b9aa9e3729eabf92af41e8be4620f53b36b2fef
yunya000 <tsaiyunyaaa@gmail.com>
2024-12-04 23:37:13 +0800

    1204-1

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let text = document.getElementById('text');
+let p11= document.getElementById('p1-1');
 let p2  = document.getElementById('p2');
 let p3  = document.getElementById('p3');
 let p4  = document.getElementById('p4');
 let p5  = document.getElementById('p5');
 let p6  = document.getElementById('p6');
+let p61  = document.getElementById('p6-1');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
@@ -13,4 +15,6 @@ window.addEventListener('scroll', () => {
     p4.style.marginTop = value * 0.6 + 'px';
     p5.style.marginTop = value * 0.9 + 'px';
     p6.style.marginTop = value * 1+ 'px';
+    p11.style.marginTop = value * 1+ 'px';
+
 });
