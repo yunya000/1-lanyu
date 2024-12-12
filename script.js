0b37ba45dec5383272f52a3db017bb26c834e44b
yunya000 <tsaiyunyaaa@gmail.com>
2024-12-12 23:28:15 +0800

    1212-2

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     // 調整每個圖片的滾動速度
-    text.style.marginTop = value * 0.3 + 'px';  // 文字移動速度較慢
+    text.style.marginTop = value * 0.9 + 'px';  // 文字移動速度較慢
     p2.style.marginTop = value * 0.1 + 'px';  // 這層移動較慢
     p3.style.marginTop = value * 0.2 + 'px';  // 中等速度
     p4.style.marginTop = value * 0.3 + 'px';  // 中等速度
     p5.style.marginTop = value * 0.5 + 'px';  // 快速
     p6.style.marginTop = value * 0.7 + 'px';  // 更快速
     p7.style.marginTop = value * 1 + 'px';    // 最快速
-    p11.style.marginTop = value * 1.2 + 'px'; // 最快速的元素
+    
 });
